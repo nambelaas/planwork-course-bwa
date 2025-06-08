a281160c1f51199446fecde9239a96511a00baeb
nambelaas <salmaniseif@gmail.com>
2025-06-08 11:50:24 +0700

    active menu

--- resources/js/Layouts/Partials/Sidebar.jsx
+++ resources/js/Layouts/Partials/Sidebar.jsx
@@ -1,8 +1,9 @@
 import { Avatar, AvatarFallback } from '@/Components/ui/avatar';
+import { cn } from '@/lib/utils';
 import { Link } from '@inertiajs/react';
 import { PiHouse, PiLockKeyOpen, PiPlus, PiSquaresFour, PiUser } from 'react-icons/pi';
 
-export default function Sidebar({ auth }) {
+export default function Sidebar({ auth, url }) {
     return (
         <nav className="flex flex-1 flex-col">
             <ul role="list" className="flex flex-1 flex-col gap-y-7">
@@ -10,26 +11,74 @@ export default function Sidebar({ auth }) {
                     <ul role="list" className="-mx-2 space-y-2">
                         {/* menu */}
                         <li>
-                            <Link className="group flex gap-x-3 rounded-md p-3 text-sm font-semibold leading-relaxed tracking-tighter text-foreground hover:bg-gray-100">
-                                <PiHouse className="h-6 w-6 shrink-0 text-foreground" />
+                            <Link
+                                className={cn(
+                                    url.startsWith('/dashboard')
+                                        ? 'bg-red-500 text-white'
+                                        : 'text-foreground hover:bg-gray-100',
+                                    'group flex gap-x-3 rounded-md p-3 text-sm font-semibold leading-relaxed',
+                                )}
+                            >
+                                <PiHouse
+                                    className={cn(
+                                        url.startsWith('/dashboard') ? 'text-white' : 'text-foreground',
+                                        'h-6 w-6 shrink-0',
+                                    )}
+                                />
                                 Dashboard
                             </Link>
                         </li>
                         <li>
-                            <Link className="group flex gap-x-3 rounded-md p-3 text-sm font-semibold leading-relaxed tracking-tighter text-foreground hover:bg-gray-100">
-                                <PiUser className="h-6 w-6 shrink-0 text-foreground" />
+                            <Link
+                                className={cn(
+                                    url.startsWith('/users')
+                                        ? 'bg-red-500 text-white'
+                                        : 'text-foreground hover:bg-gray-100',
+                                    'group flex gap-x-3 rounded-md p-3 text-sm font-semibold leading-relaxed',
+                                )}
+                            >
+                                <PiUser
+                                    className={cn(
+                                        url.startsWith('/users') ? 'text-white' : 'text-foreground',
+                                        'h-6 w-6 shrink-0',
+                                    )}
+                                />
                                 People
                             </Link>
                         </li>
                         <li>
-                            <Link className="group flex gap-x-3 rounded-md p-3 text-sm font-semibold leading-relaxed tracking-tighter text-foreground hover:bg-gray-100">
-                                <PiSquaresFour className="h-6 w-6 shrink-0 text-foreground" />
+                            <Link
+                                className={cn(
+                                    url.startsWith('/my-tasks')
+                                        ? 'bg-red-500 text-white'
+                                        : 'text-foreground hover:bg-gray-100',
+                                    'group flex gap-x-3 rounded-md p-3 text-sm font-semibold leading-relaxed',
+                                )}
+                            >
+                                <PiSquaresFour
+                                    className={cn(
+                                        url.startsWith('/my-tasks') ? 'text-white' : 'text-foreground',
+                                        'h-6 w-6 shrink-0',
+                                    )}
+                                />
                                 My Task
                             </Link>
                         </li>
                         <li>
-                            <Link className="group flex gap-x-3 rounded-md p-3 text-sm font-semibold leading-relaxed tracking-tighter text-foreground hover:bg-gray-100">
-                                <PiLockKeyOpen className="h-6 w-6 shrink-0 text-foreground" />
+                            <Link
+                                className={cn(
+                                    url.startsWith('/logout')
+                                        ? 'bg-red-500 text-white'
+                                        : 'text-foreground hover:bg-gray-100',
+                                    'group flex gap-x-3 rounded-md p-3 text-sm font-semibold leading-relaxed',
+                                )}
+                            >
+                                <PiLockKeyOpen
+                                    className={cn(
+                                        url.startsWith('/logout') ? 'text-white' : 'text-foreground',
+                                        'h-6 w-6 shrink-0',
+                                    )}
+                                />
                                 Logout
                             </Link>
                         </li>
